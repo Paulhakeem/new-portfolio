608116af9e1f375d515a3262b5a6ea468819ae29
Paul <paulnyamawi18@gmail.com>
2025-07-13 18:37:04 +0300

    done with contact page

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,13 +1,117 @@
 <template>
-    <div>
-helo
+  <main class="flex gap-4 pt-10 mx-10 justify-center items-center m-auto">
+    <div
+      data-aos="fade-up-right"
+      data-aos-anchor-placement="top-center"
+      class="bg-[#ff4b57] p-6 rounded-lg w-full mx-2"
+    >
+      <h2 class="text-lg font-semibold mb-4 text-gray-300">
+        Contact information
+      </h2>
+      <div class="flex items-center mb-4">
+        <Icon
+          name="line-md:phone-filled"
+          class="text-gray-200 mr-2"
+          size="32"
+        />
+        <div>
+          <p class="text-sm text-gray-300 font-semibold">+254 792 857 288</p>
+          <p class="text-sm text-gray-300 font-semibold">+254 759 732 432</p>
+        </div>
+      </div>
+      <div class="flex items-center mb-4">
+        <Icon
+          name="line-md:email-filled"
+          class="text-gray-200 mr-2"
+          size="32"
+        />
+        <div>
+          <p class="text-sm text-gray-300 font-semibold">
+            paulnyamawi18@gmail.com
+          </p>
+          <p class="text-sm text-gray-300 font-semibold">
+            poltechnology01@gmail.com
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-center mb-4">
+        <Icon
+          name="tdesign:location-filled"
+          class="text-gray-200 mr-2"
+          size="32"
+        />
+        <div>
+          <p class="text-sm text-gray-300 font-semibold">Mombasa Kenya</p>
+        </div>
+      </div>
     </div>
+
+    <div
+      data-aos="fade-up-left"
+      data-aos-anchor-placement="top-center"
+      class="p-6 w-full"
+    >
+      <form>
+        <div class="flex gap-10 w-full">
+          <div class="mb-4">
+            <label for="name" class="block text-sm font-medium text-gray-300"
+              >Name</label
+            >
+            <input
+              type="text"
+              id="name"
+              class="text-gray-400 mt-1 block w-full p-2 border-0 border-b-2 border-b-gray-300 focus:border-b-[#ff4b57] focus:ring-0 focus:outline-none transition duration-200"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div class="mb-4">
+            <label for="name" class="block text-sm font-medium text-gray-300"
+              >Email</label
+            >
+            <input
+              type="email"
+              id="email"
+              class="text-gray-400 mt-1 block w-full p-2 border-0 border-b-2 border-b-gray-300 focus:border-b-[#ff4b57] focus:ring-0 focus:outline-none transition duration-200"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+        </div>
+        <div class="mb-4 w-full pt-4">
+          <label for="message" class="block text-sm font-medium text-gray-300"
+            >Your Subject</label
+          >
+          <input
+            type="text"
+            id="message"
+            class="w-full mt-2 p-2 text-sm text-gray-400 placeholder-gray-400 border-0 border-b-2 border-b-gray-300 focus:border-b-[#ff4b57] focus:ring-0 focus:outline-none transition duration-200"
+            placeholder="Your subject"
+            required
+          />
+        </div>
+
+        <div class="mb-4 w-full pt-4">
+          <label for="message" class="block text-sm font-medium text-[#ff4b57]"
+            >Message</label
+          >
+          <textarea
+            id="message"
+            class="w-full mt-2 p-2 text-sm text-gray-400 placeholder-gray-400 border-0 border-b-2 border-b-gray-300 focus:border-b-[#ff4b57] focus:ring-0 focus:outline-none transition duration-200"
+            placeholder="Your Message"
+            required
+          ></textarea>
+        </div>
+
+        <div class="mt-6">
+          <Button class="bg-[#ff4b57] cursor-pointer">Send Message</Button>
+        </div>
+      </form>
+    </div>
+</main>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

5dd3ad397515ad1d5d5c893b34a6342dc48cc2f6
Paul <paulnyamawi18@gmail.com>
2025-11-19 22:31:16 +0300

    done

--- components/Project/projects.vue
+++ components/Project/projects.vue
@@ -20,104 +20,15 @@
 
             <a
               :href="img.link"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit" target="_blank"
+              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
+              target="_blank"
             >
               View Project
             </a>
           </div>
         </div>
       </div>
-
-      <!-- DUPLICATE FOR SEAMLESS SCROLL -->
-      <div class="flex animate-slide">
-        <div class="relative w-80 mx-4 rounded-xl overflow-hidden">
-          <img
-            src="/images/My holiday deals.png"
-            class="w-full h-full object-cover"
-          />
-
-          <div
-            class="absolute inset-0 bg-black/40 flex flex-col justify-end p-4"
-          >
-            <h3 class="text-white text-lg font-bold">Holiday Deals</h3>
-            <p class="text-gray-200 text-sm mb-2">Offers & travel UI</p>
-            <a
-              href="#"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
-            >
-              View Project
-            </a>
-          </div>
-        </div>
-
-        <div class="relative w-80 mx-4 rounded-xl overflow-hidden">
-          <img src="/images/6.png" class="w-full h-full object-cover" />
-
-          <div
-            class="absolute inset-0 bg-black/40 flex flex-col justify-end p-4"
-          >
-            <h3 class="text-white text-lg font-bold">Social Media Graphic</h3>
-            <p class="text-gray-200 text-sm mb-2">High-quality visuals</p>
-            <a
-              href="#"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
-            >
-              View Project
-            </a>
-          </div>
-        </div>
-
-        <div class="relative w-80 mx-4 rounded-xl overflow-hidden">
-          <img src="/images/3.png" class="w-full h-full object-cover" />
-
-          <div
-            class="absolute inset-0 bg-black/40 flex flex-col justify-end p-4"
-          >
-            <h3 class="text-white text-lg font-bold">Brand Concept</h3>
-            <p class="text-gray-200 text-sm mb-2">Sleek & elegant</p>
-            <a
-              href="#"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
-            >
-              View Project
-            </a>
-          </div>
-        </div>
-
-        <div class="relative w-80 mx-4 rounded-xl overflow-hidden">
-          <img src="/images/5.png" class="w-full h-full object-cover" />
-
-          <div
-            class="absolute inset-0 bg-black/40 flex flex-col justify-end p-4"
-          >
-            <h3 class="text-white text-lg font-bold">Promo Banner</h3>
-            <p class="text-gray-200 text-sm mb-2">Bold & stylish</p>
-            <a
-              href="#"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
-            >
-              View Project
-            </a>
-          </div>
-        </div>
-
-        <div class="relative w-80 mx-4 rounded-xl overflow-hidden">
-          <img src="/images/4.png" class="w-full h-full object-cover" />
-
-          <div
-            class="absolute inset-0 bg-black/40 flex flex-col justify-end p-4"
-          >
-            <h3 class="text-white text-lg font-bold">Mockup Design</h3>
-            <p class="text-gray-200 text-sm mb-2">Clean & professional</p>
-            <a
-              href="#"
-              class="px-3 py-1 bg-[#ff4b57] text-white text-sm rounded-md w-fit"
-            >
-              View Project
-            </a>
-          </div>
-        </div>
-      </div>
+      <!-- dublicate -->
     </div>
   </section>
 </template>
